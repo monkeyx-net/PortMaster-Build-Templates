ea1addecc156ba0a243ef16746840d56676059d9
monekyx-net <tim@monkeyx.net>
2025-12-14 23:02:14 +0000

    Test build

--- recipes/new_recipe.sh
+++ recipes/new_recipe.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+GITHUB_URL="https://github.com/monkeyx-net/PortMaster-Build-Templates/actions/workflows/buildrecipe.yml"
+
 exit_nicely () {
   tput sgr0
   exit
+}
+
+read_check() {
+    local prompt="$1"
+    local var_name="$2"
+
+    while true; do
+        read -rp "$prompt" input
+        if [ -n "$input" ]; then
+            eval "$var_name='$input'"
+            break
+        else
+            echo "Input cannot be empty. Please try again."
+        fi
+    done
 }
 
 create_recipe (){
@@ -47,7 +64,8 @@ update_port_json() {
         .items[0] = $port_bash |
         .items[1] = $port_folder |
         .attr.title = $port_title |
-        .attr.porter[0] = $porter_name' new_ports/${port_folder}/${port_folder}/port.json > new_ports/${port_folder}/${port_folder}/port.tmp && mv new_ports/${port_folder}/${port_folder}/port.tmp new_ports/${port_folder}/${port_folder}/port.json
+        .attr.porter[0] = $porter_name' new_ports/${port_folder}/${port_folder}/port.json > new_ports/${port_folder}/${port_folder}/port.tmp
+        mv new_ports/${port_folder}/${port_folder}/port.tmp new_ports/${port_folder}/${port_folder}/port.json
 }
 
 create_new_port () {
@@ -67,11 +85,11 @@ create_new_port () {
   esac
   echo -e "\nCreating a recipe file for port title: ${port_title}"
   echo -e "This will create basic files for a new port"
-  read -rp "Enter PortMaster porter name ie github, discord or nickname for your port: " porter_name
-  read -rp "Enter port executable name: " port_exe
-  read -rp "Enter zip/folder name: " port_folder
+  read_check "Enter PortMaster porter name ie github, discord or nickname for your port: " porter_name
+  read_check "Enter port executable name: " port_exe
+  read_check "Enter zip/folder name: " port_folder
   folder_name=$(echo "$port_folder" | tr '[:upper:]' '[:lower:]')
-  read -rp "Enter Bash file name (include .sh): " port_bash
+  read_check "Enter Bash file name (include .sh): " port_bash
   if [[ "${port_bash: -3}" != ".sh" ]]; then
     port_bash="${port_bash}.sh"
     echo "Added .sh extension: $port_bash"
@@ -80,13 +98,13 @@ create_new_port () {
   cp recipes/templates/port_template/zz_Port.sh new_ports/${port_folder}/${port_bash}
   sed -i 's/zz_port/'"${port_exe}"'/g; s/zz_folder/'"${port_folder}"'/g' new_ports/${port_folder}/${port_bash}
   cp -r recipes/templates/port_template/zz_folder/ new_ports/${port_folder}/${port_folder}
-  mv new_ports/${port_folder}/${port_folder}/zz_folder.gptk new_ports/${port_folder}/${port_folder}/${port_exe}.gtpk
+  mv new_ports/${port_folder}/${port_folder}/zz_folder.gptk new_ports/${port_folder}/${port_folder}/${port_exe}.gptk
   mv new_ports/${port_folder}/${port_folder}/zz_folder_LICENSE.txt new_ports/${port_folder}/${port_folder}/${port_exe}_LICENSE.txt
-  mv new_ports/${port_folder}/${port_folder}/zz_folder.md new_ports/${port_folder}/${port_folder}/${port_exe}.md
+  mv new_ports/${port_folder}/${port_folder}/README.md new_ports/${port_folder}/${port_folder}/README.md
   update_port_json "${port_title}" "${porter_name}" "${port_folder}" "${port_bash}"
   while true; do
     echo "Enter URL for port code base. Must be in .zip or .tar.gz format"
-    read -rp 'Enter URL for code: ' port_url
+    read_check 'Enter URL for code: ' port_url
     if [ -z "${port_url}" ]; then
       echo "Empty input received. Please enter a valid URL."
     elif [[ "${port_url}" =~ \.zip$ || "${port_url}" =~ \.tar\.gz$ ]]; then
@@ -104,13 +122,18 @@ create_new_port () {
     fi
   done
   # TODO seperate build commands? shared libs?
-  read -rp 'Enter build command(s): ' port_build
+  read_check 'Enter build command(s): ' port_build
   create_recipe "${port_folder}" "${port_url}" "${port_build}" "${port_checksum}" "${port_exe}"
   echo -e "\nCreated recipe for ${port_folder}_recipe.json\n"
   cat recipes/files/${port_folder}_recipe.json | jq '.'
   echo -e "\nCreated files/folders for ${port_folder}\n"
   tree new_ports/${port_folder}/
-  xdg-open "https://github.com/monkeyx-net/PortMaster-Build-Templates/actions/workflows/buildrecipe.yml"  2>/dev/null
+  echo -e "\nBefore disappearing to github. Please review the files created, add screenshots etc\n"
+  echo -e "\nPlease refer to this guide for further information - https://portmaster.games/porting.html"
+  echo -e "\nAfter making the changes do a git add, commit, push"
+  echo -e "\nOpening github ${GITHUB_URL} to run workflow"
+  xdg-open $GITHUB_URL  2>/dev/null
+
   exit_nicely
 }
 
@@ -155,7 +178,7 @@ fi
 
 echo "Available ports with ${port_title} in the title:"
 for i in "${!ports[@]}"; do
-    echo "$((i + 1))) ${ports[i]}"
+    echo "$((i + 1)) ${ports[i]}"
 done
 
 while true; do
