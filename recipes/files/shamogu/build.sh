25bd7dfd8dc102dea4ab483556a48d94ca15a686
monekyx-net <tim@monkeyx.net>
2026-01-25 15:02:57 +0000

    New stuff

--- recipes/files/shamogu/build.sh
+++ recipes/files/shamogu/build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+
+set -e
+
 PORT_FOLDER="$1"
 PORT_BUILD="$2"
 PORT_EXE="$3"
 ARCH="$4"
 
-
-
 echo ${PORT_FOLDER}
-
-#pwd
-#ls -lha
-#cd "new_ports/build/${PORT_FOLDER}"
-#pwd
-#ls -lha
-#${PORT_BUILD}
-#mkdir -p dist
-#cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
-#strip "dist/${PORT_EXE}.${ARCH}" || true
-#tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C dist .
+echo ${PORT_BUILD}
+cd "new_ports/build/${PORT_FOLDER}"
+${PORT_BUILD}
+mkdir -p dist
+cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
+strip "dist/${PORT_EXE}.${ARCH}" || true
+tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C dist .
+pwd
+ls -lha
