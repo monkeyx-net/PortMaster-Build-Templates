f2b0caa0277be300b5b5b63b0d28e850dea118cf
monekyx-net <tim@monkeyx.net>
2026-02-02 14:55:18 +0000

    Missing lib defenguin

--- new_ports/defendguin/Defendguin.sh
+++ new_ports/defendguin/Defendguin.sh
@@ -22,12 +22,12 @@ GAMEDIR=/$directory/ports/defendguin
 exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 cd $GAMEDIR
-controls/constrols.${DEVICE_ARCH} controls/controls.png
+controls/controls.${DEVICE_ARCH} controls/controls.png
 # Needed if config data binding to the conf folder
 bind_directories ~/.local/share/$GAMEBINARY $GAMEDIR/conf
 # Needed if any extra libs addded.
 export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 
 $GPTOKEYB "$GAMEBINARY.${DEVICE_ARCH}" -c "./$GAMEBINARY.gptk" &
-SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./$GAMEBINARY.${DEVICE_ARCH} --width 800 --height 480
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./$GAMEBINARY.${DEVICE_ARCH} --fullscreen --width 800 --height 480
 pm_finish
