2bc432cd48e7d1e6291d66e52daa35f5dbdae8f7
monekyx-net <tim@monkeyx.net>
2026-02-02 15:44:35 +0000

    SOH test

--- recipes/templates/scripts/build.sh
+++ recipes/templates/scripts/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 PORT_FOLDER="$1"
 PORT_BUILD="$2"
 PORT_EXE="$3"
 ARCH="$4"
-DEST_DIR="${PORT_FOLDER}/libs.${ARCH}"
+DEST_DIR="dist/libs.${ARCH}"
 FILES=(
     "libfluidsynth.so.2"
     "libinstpatch-1.0.so.2"
@@ -17,14 +17,26 @@ FILES=(
     "libSDL_mixer-1.2.so.0"
     "libSDL-1.2.so.0"
     "libtinfo.so.6"
+    "libFLAC.so.8"
 )
 
 
-if [[${ARCH} == "aarch64"]]; then
+if [[ ${ARCH} ==  "aarch64" ]]; then
   SOURCE_DIR="/usr/lib/aarch64-linux-gnu/"
-elif [[${ARCH} == "x86_64"]]; then
+elif [[ ${ARCH} == "x86_64" ]]; then
   SOURCE_DIR="/usr/lib/x86_64-linux-gnu/"
 fi
+
+cd "new_ports/build/${PORT_FOLDER}"
+${PORT_BUILD}
+mkdir -p dist/libs.${ARCH}
+cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
+strip "dist/${PORT_EXE}.${ARCH}" || true
+
+# add if folder?
+cp -r data/ dist/
+
+#get license file
 
 # if sourcedir !null and files !null
 for file in "${FILES[@]}"; do
@@ -32,13 +44,6 @@ for file in "${FILES[@]}"; do
 done
 
 
-echo ${PORT_FOLDER}
-echo ${PORT_BUILD}
-cd "new_ports/build/${PORT_FOLDER}"
-${PORT_BUILD}
-mkdir -p dist
-cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
-strip "dist/${PORT_EXE}.${ARCH}" || true
 tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C dist .
 pwd
 ls -lha
