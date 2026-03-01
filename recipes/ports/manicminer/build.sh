7609de429250006354c5b1fe54b9229630e687b2
monekyx-net <tim@monkeyx.net>
2026-03-01 00:25:28 +0000

    Documents

---
+++ recipes/ports/manicminer/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+PORT_FOLDER="$1"
+PORT_BUILD="$2"
+PORT_EXE="$3"
+ARCH="$4"
+DEST_DIR="dist/libs.${ARCH}"
+FILES=(
+    "libfluidsynth.so.2"
+    "libinstpatch-1.0.so.2"
+    "libjack.so.0"
+    "libmad.so.0"
+    "libmikmod.so.3"
+    "libreadline.so.8"
+    "libSDL_mixer-1.2.so.0"
+    "libSDL-1.2.so.0"
+    "libtinfo.so.6"
+    "libFLAC.so.8"
+)
+
+
+if [[ ${ARCH} ==  "aarch64" ]]; then
+  SOURCE_DIR="/usr/lib/aarch64-linux-gnu/"
+elif [[ ${ARCH} == "x86_64" ]]; then
+  SOURCE_DIR="/usr/lib/x86_64-linux-gnu/"
+fi
+
+cd "new_ports/${PORT_FOLDER}/source"
+${PORT_BUILD}
+mkdir -p dist/libs.${ARCH}
+cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
+strip "dist/${PORT_EXE}.${ARCH}" || true
+
+# add if folder?
+cp -r data/ dist/
+
+#get license file
+
+# if sourcedir !null and files !null
+for file in "${FILES[@]}"; do
+    cp "${SOURCE_DIR}/${file}" "${DEST_DIR}/" 2>/dev/null || echo "Warning: ${file} not found"
+done
+
+
+tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C dist .
+pwd
+ls -lha
