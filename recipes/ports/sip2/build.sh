356b773f7156940005d6338f31099d418352845c
monekyx-net <tim@monkeyx.net>
2026-03-02 18:15:57 +0000

    Wider SIP2

--- recipes/ports/sip2/build.sh
+++ recipes/ports/sip2/build.sh
@@ -9,6 +9,7 @@ ARCH="$4"
 
 cd "new_ports/${PORT_FOLDER}/source"
 ${PORT_BUILD}
+mkdir -p dist
 cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
 strip "dist/${PORT_EXE}.${ARCH}" || true
 cp engine.config dist/
