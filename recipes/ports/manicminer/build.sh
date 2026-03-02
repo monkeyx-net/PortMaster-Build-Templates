3f3873b11e24ceb87659172396e3eb081e7c8202
monekyx-net <tim@monkeyx.net>
2026-03-02 18:37:24 +0000

    Arbitrarly wider

--- recipes/ports/manicminer/build.sh
+++ recipes/ports/manicminer/build.sh
@@ -6,11 +6,11 @@ PORT_FOLDER="$1"
 PORT_BUILD="$2"
 PORT_EXE="$3"
 ARCH="$4"
-DEST_DIR="dist/libs.${ARCH}"
 
 
 cd "new_ports/${PORT_FOLDER}/source"
 ${PORT_BUILD}
+mkdir -p dist
 cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
 strip "dist/${PORT_EXE}.${ARCH}" || true
 
--- recipes/ports/shamogu/build.sh
+++ recipes/ports/shamogu/build.sh
@@ -6,15 +6,6 @@ PORT_FOLDER="$1"
 PORT_BUILD="$2"
 PORT_EXE="$3"
 ARCH="$4"
-DEST_DIR="${PORT_FOLDER}/libs.${ARCH}"
-
-if [[ ${ARCH} == "aarch64" ]]; then
-  SOURCE_DIR="/usr/lib/aarch64-linux-gnu/"
-elif [[ ${ARCH} == "x86_64" ]]; then
-  SOURCE_DIR="/usr/lib/x86_64-linux-gnu/"
-else
-  echo "Have you chosen your ${ARCH} yet!"
-fi
 
 echo ${PORT_FOLDER}
 echo ${PORT_BUILD}
