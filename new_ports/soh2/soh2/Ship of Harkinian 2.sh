dcc6481d41ae77e35a7022cc3cb2afed368fcb39
monekyx-net <tim@monkeyx.net>
2026-03-10 20:16:02 +0000

    soh2 v1.1

---
+++ new_ports/soh2/soh2/Ship of Harkinian 2.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Set variables
+GAMEDIR="/$directory/ports/soh2"
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG=$sdl_controllerconfig
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x "$GAMEDIR/2s2h.elf"
+$ESUDO chmod +x "$GAMEDIR/assets/otrgen"
+
+# Close the menu if open
+sed -i 's/"Menu": *1/"Menu": 0/' 2ship2harkinian.json
+
+# -------------------- BEGIN FUNCTIONS --------------------
+
+# Check imgui.ini and modify if needed
+imgui_reset() {
+    input_file="imgui.ini"
+    temp_file="imgui_temp.ini"
+    skip_section=0
+    # Loop through each line in the input file
+    while IFS= read -r line; do
+        # Check if the line is a window header
+        if [[ "$line" =~ ^\[Window\]\[Main\ Game\] || "$line" =~ ^\[Window\]\[Main\ -\ Deck\] ]]; then
+            skip_section=1  # Set the flag to skip modifications for this section
+        elif [[ "$line" =~ ^\[Window\] ]]; then
+            skip_section=0  # Reset the flag for other windows
+        fi
+
+        # Modify Pos and Size only if the current section is not skipped
+        if [[ $skip_section -eq 0 ]]; then
+            if [[ "$line" =~ ^Pos=.* ]]; then
+                echo "Pos=30,30" >> "$temp_file"
+            elif [[ "$line" =~ ^Size=.* ]]; then
+                echo "Size=400,300" >> "$temp_file"
+            else
+                echo "$line" >> "$temp_file"
+            fi
+        else
+            # If skipping, write the line unchanged
+            echo "$line" >> "$temp_file"
+        fi
+    done < "$input_file"
+
+    # Replace the original file with the modified one
+    mv "$temp_file" "$input_file"
+}
+
+o2r_check() {
+
+# Warn if mm.o2r is older than 2s2h.elf or 2ship.o2r
+if [ -f "$GAMEDIR/mm.o2r" ]; then
+    if [ -f "$GAMEDIR/2s2h.elf" ] && [ "$GAMEDIR/2s2h.elf" -nt "$GAMEDIR/mm.o2r" ] \
+       || [ -f "$GAMEDIR/2ship.o2r" ] && [ "$GAMEDIR/2ship.o2r" -nt "$GAMEDIR/mm.o2r" ]; then
+        echo "Notice: mm.o2r is older than 2s2h.elf and/or 2ship.o2r. Forcing regeneration."
+        rm -f "$GAMEDIR/mm.o2r"
+        REGEN=1
+        export REGEN
+    fi
+fi
+
+if [ ! -f "mm.o2r" ]; then
+    # Ensure we have a rom file before attempting to generate o2r
+    if ls "$GAMEDIR/baseroms/"*.*64 1> /dev/null 2>&1; then
+        if [ -f "$controlfolder/utils/patcher.txt" ]; then
+            export PATCHER_FILE="$GAMEDIR/assets/otrgen"
+            export PATCHER_GAME="$(basename "${0%.*}")"
+            export PATCHER_TIME="5 to 10 minutes"
+            export controlfolder
+            export DEVICE_ARCH
+            source "$controlfolder/utils/patcher.txt"
+            $ESUDO kill -9 $(pidof gptokeyb)
+        else
+            pm_message "This port requires the latest version of PortMaster."
+        fi
+    else
+        echo "Missing ROM files in $GAMEDIR/baseroms! Can't generate o2r!"
+        exit 1
+    fi
+    
+    # Check if OTR files were generated
+    if [ ! -f "mm.o2r" ]; then
+        echo "No o2r files, can't run the game!"
+        exit 1
+    fi
+fi
+}
+
+# --------------------- END FUNCTIONS ---------------------
+
+# Perform functions
+o2r_check
+
+if [ -f "imgui.ini" ]; then
+    imgui_reset
+fi
+
+# Run the game
+$GPTOKEYB "2s2h.elf" -c "soh2.gptk" & 
+pm_platform_helper "2s2h.elf" >/dev/null
+./2s2h.elf
+
+# Cleanup
+rm -rf "$GAMEDIR/logs"
+pm_finish
---
+++ recipes/ports/soh2/build.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+#set -e
+
+PORT_FOLDER="$1"
+PORT_BUILD="$2"
+PORT_EXE="$3"
+ARCH="$4"
+# needs file.arch?
+FILES=(
+  "libz.so.1"
+  "libbz2.so.1"
+  "libbz2.so.1.0"
+  "libopus.so.0"
+  "libopusfile.so.0"
+  "libogg.so.0"
+  "libpng16.so.16"
+  "libspdlog.so.1"
+  "libspdlog.so.1.12"
+  "libzip.so.5"
+  "libtinyxml2.so.10"
+)
+CDIR=$(pwd)
+DEST_DIR="${CDIR}/dist/libs.${ARCH}"
+
+if [[ ${ARCH} ==  "aarch64" ]]; then
+  SOURCE_DIR="/usr/lib/aarch64-linux-gnu"
+elif [[ ${ARCH} == "x86_64" ]]; then
+  SOURCE_DIR="/usr/lib/x86_64-linux-gnu"
+fi
+
+#### SoH ####
+git clone https://github.com/HarbourMasters/2ship2harkinian.git
+cd 2ship2harkinian
+
+git fetch --tags
+git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+
+git submodule update --init
+
+cd libultraship
+patch -p1 < /recipes/ports/soh2/soh2.patch
+cd 
+
+export CFLAGS="-O3 -Wall -Wextra -Wno-return-type -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-macro-redefined -Wno-unknown-warning-option"
+export CXXFLAGS="-O3 -Wall -Wextra -Wno-return-type -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-macro-redefined -Wno-unknown-warning-option"
+
+# Verify where clang is or use the default 'clang' which is v18 on 24.04
+if ! command -v clang-18 &> /dev/null; then
+    export CC=clang
+    export CXX=clang++
+else
+    export CC=clang-18
+    export CXX=clang++-18
+fi
+
+mkdir build-soh2 && cd build-soh2
+cmake .. -GNinja -DUSE_OPENGLES=1 -DBUILD_CROWD_CONTROL=0 -DCMAKE_BUILD_TYPE=Release
+cmake --build . -j8
+cmake --build . --target Generate2ShipOtr -j8
+cd ../..
+mkdir -p ${CDIR}/dist/assets/extractor
+ls -lha 2ship2harkinian/build-soh2/mm/
+strip "2ship2harkinian/build-soh2/mm/${PORT_EXE}.elf" || true
+cp "2ship2harkinian/build-soh2/mm/${PORT_EXE}.elf" "dist/${PORT_EXE}.elf.${ARCH}"
+cp "2ship2harkinian/build-soh2/mm/2ship.o2r" "dist/"
+cp "2ship2harkinian/build-soh2/ZAPD/ZAPD.out" "${CDIR}/dist/assets/extractor/ZAPD.out"
+ls -lha dist/
+cd dist/assets
+mkdir assets_zip
+cp -r "${CDIR}/2ship2harkinian/mm/assets/extractor/." assets_zip/
+cp -r "${CDIR}/2ship2harkinian/mm/assets/xml/" assets_zip/
+cd assets_zip
+zip -r ../extractor.zip ./*
+cd ..
+rm -rf assets_zip
+
+#get license file
+
+mkdir -p ${CDIR}/dist/libs.${ARCH}
+# if sourcedir !null and files !null
+for file in "${FILES[@]}"; do
+    cp "${SOURCE_DIR}/${file}" "${DEST_DIR}/" 2>/dev/null || echo "Warning: ${file} not found"
+done
+
+tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C ${CDIR}/dist .
+pwd
+ls -lha
