3143fb06b993c7ed777bfebdbfaa73405acb599e
monekyx-net <tim@monkeyx.net>
2026-03-11 17:14:02 +0000

    soh2.7

--- recipes/ports/soh2/build.sh
+++ recipes/ports/soh2/build.sh
@@ -65,19 +65,13 @@ strip "2ship2harkinian/build-soh2/mm/${PORT_EXE}.elf" || true
 cp "2ship2harkinian/build-soh2/mm/${PORT_EXE}.elf" "dist/${PORT_EXE}.elf.${ARCH}"
 cp "2ship2harkinian/build-soh2/mm/2ship.o2r" "dist/"
 cp "2ship2harkinian/build-soh2/ZAPD/ZAPD.out" "${CDIR}/dist/assets/extractor/ZAPD.out"
-cp -r "${CDIR}/2ship2harkinian/mm/assets/extractor/." "${CDIR}/dist/assets/"
-cp -r "${CDIR}/2ship2harkinian/mm/assets/xml/." "${CDIR}/dist/assets/"
+cp -r "${CDIR}/2ship2harkinian/mm/assets/extractor/." "${CDIR}/dist/assets/extractor/"
+cp -r "${CDIR}/2ship2harkinian/mm/assets/xml/." "${CDIR}/dist/assets/xml/"
 ls -lha dist/
-#cd dist/assets
-#mkdir assets_zip
-#cp -r "${CDIR}/2ship2harkinian/mm/assets/extractor/." assets_zip/
-#cp -r "${CDIR}/2ship2harkinian/mm/assets/xml/" assets_zip/
-#cd assets_zip
 cd "${CDIR}/dist/assets"
-zip -r ../extractor.zip ./*
-#rm -rf assets_zip
-
-#get license file
+zip -r ${CDIR}/dist/extractor.zip ./*
+cd ..
+rm -rf assets
 
 mkdir -p ${CDIR}/dist/libs.${ARCH}
 # if sourcedir !null and files !null
