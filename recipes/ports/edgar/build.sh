0432ec9407d1efc9980c6ca516d663c7b5b04588
monekyx-net <tim@monkeyx.net>
2026-03-27 17:26:44 +0000

    Edgar

---
+++ recipes/ports/edgar/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#set -e
+
+PORT_FOLDER="$1"
+PORT_BUILD="$2"
+PORT_EXE="$3"
+ARCH="$4"
+DEST_DIR="dist/libs.${ARCH}"
+
+
+cd "new_ports/${PORT_FOLDER}/source"
+${PORT_BUILD}
+mkdir -p dist/libs.${ARCH}
+cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
+strip "dist/${PORT_EXE}.${ARCH}" || true
+
+tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C dist .
