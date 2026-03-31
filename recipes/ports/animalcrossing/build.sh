b54eae9e4bec786703b91ac8088244e5998f318b
monekyx-net <tim@monkeyx.net>
2026-03-31 18:57:00 +0100

    Animal Crossing

--- recipes/ports/animalcrossing/build.sh
+++ recipes/ports/animalcrossing/build.sh
@@ -12,8 +12,10 @@ DEST_DIR="dist/libs.${ARCH}"
 cd "new_ports/${PORT_FOLDER}/source"
 ${PORT_BUILD}
 mkdir -p dist/shaders
-cp "${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
+cp "bin/${PORT_EXE}" "dist/${PORT_EXE}.${ARCH}"
 strip "dist/${PORT_EXE}.${ARCH}" || true
-cp -r ../pc/shaders/* dist/shaders/
+cp -r bin/shaders/* dist/shaders/
+ls -lha
+ls -lha bin
 
 tar -czf "/workspace/${PORT_FOLDER}-linux-${ARCH}.tar.gz" -C dist .
